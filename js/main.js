73200488af014c707ae51535c13a55ea655a7bc5
Josueargumedo <70778546+J-Argumedo-code@users.noreply.github.com>
2021-03-28 14:45:55 -0600

    Add animations

---
+++ js/main.js
@@ -0,0 +1,20 @@
+window.addEventListener('scroll', function(){
+    let animacion = document.getElementById('imagenes');
+    let posicion = animacion.getBoundingClientRect().top;
+    let tamañoPantalla = window.innerHeight/1.2
+    if(posicion < tamañoPantalla){
+        animacion.style.opacity = '100%'
+        animacion.style.transform = 'translate(0)'
+    } else if(posicion > tamañoPantalla * 1.2){
+        animacion.style.transform = 'translateX(-4%)'
+        animacion.style.opacity = '0'
+    }
+})
+
+let animacion2 = document.querySelector('header');
+window.onload = animation()
+
+function animation(){
+    animacion2.style.opacity = '100%'
+    animacion2.style.transform = 'translate(0)'
+}
